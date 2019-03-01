fef8d165d67ede34d5c4365e64f4075e4c9bb06f
NeringaU <46648013+NeringaU@users.noreply.github.com>
2019-03-01 15:09:03 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,6 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.sql.SQLOutput;
 
 public class Main {
     WebDriver browser;
